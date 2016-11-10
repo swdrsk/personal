3c1c3c48ad4fdababf492bdb293761bdda42bee2
swdrsk <sawaryou325@gmail.com>
2016-11-11 05:45:05 +0900

    handmade neural net #2

--- neural_net.py
+++ neural_net.py
@@ -3,60 +3,60 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pdb
 
+
 class Layer(object):
     def __init__(self,pre,post):
         self.pre = np.zeros(pre)
         self.post = np.zeros(post)
         self.thre = np.random.rand(pre) - 0.5
-        self.weight = self.init_weight(pre,post)
+        self.weight = self.init_weight(post, pre)
 
-    def init_weight(self,pre,post):
-        return np.random.random([post,pre]) - 0.5
+    def init_weight(self, post, pre):
+        return np.random.random([post, pre]) - 0.5
 
-    def activation(self,inputs):
+    def activation(self, inputs):
         sigmoid = 1/(1+np.exp(-inputs))
         tanh = np.tanh(-inputs)
         relu = np.array(map(lambda x:(0 if x<0 else x), inputs))
         return sigmoid
-        
+
+    def inv_activation(self, inputs):
+        sigmoid = (1/(1+np.exp(-inputs)))*(1-1/(1+np.exp(-inputs)))
+        return sigmoid
+
     def out(self,inputs):
         # output = np.dot(self.weight,self.activation(inputs))+self.thre
-        output = self.activation(np.dot(self.weight,inputs)+self.thre)
+        output = self.activation(np.dot(self.weight,inputs+self.thre))
         return output
 
     def learn(self,err,learning_rate):
-        self.weight += learning_rate*(self.post-err)*self.pre
-        return np.dot(np.linalg.inv(self.weight),err)
+        for i,invpre in enumerate(self.inv_activation(self.pre)):
+            self.weight[:,i] -= learning_rate*err*self.post*invpre
+        return np.dot(np.linalg.pinv(self.weight), err)*self.inv_activation(self.pre)
+
 
 class NeuralNet(object):
     def __init__(self,*nums):
         self.layer = []
-        self.weight = []
-        for i,num in enumerate(nums):
-            self.layer[i] = Layer(num)
-            if i>0:
-                self.weight[i-1] = self.init_weight(pre_num,num)
-            prenum = num
-
-    def init_weight(x,y):
-        return np.array.random.rand([pre_num,num])*2-1
+        for pre,post in zip(nums[:-1],nums[1:]):
+            self.layer.append(Layer(pre,post))
         
-    def fit(self,inputsignal,outputsignal):
-        if inputsignal.ndim != len(self.layer[0].pre):
+    def fit(self,inputsignal,outputsignal,learning_rate):
+        if inputsignal.shape[1] != len(self.layer[0].pre):
             print("mismatch input dim vs. units num")
-        if outputsignal.ndim != len(self.layer[-1].post):
+        if outputsignal.shape[1] != len(self.layer[-1].post):
             print("mismatch output dim vs. units num")
-        if inputsignal.shape[1] != outputsignal.shape[1]:
+        if inputsignal.shape[0] != outputsignal.shape[0]:
             print("mismatch input length and output length")
         for signal,teacher in zip(inputsignal,outputsignal):
-            output = self.feedforward(inputsignal)
-            self.learn(teacher)
+            output = self.feedforward(signal)
+            self.learn(teacher-output, learning_rate)
                         
-    def learn(self,err):
+    def learn(self,err,learning_rate=0.01):
         for layer in self.layer[::-1]:
-            err = layer.learn(err)
+            err = layer.learn(err,learning_rate)
 
-    def feedforward(self,inputsignal):
+    def feedforward(self, inputsignal):
         isignal = inputsignal
         for layer in self.layer:
             osignal = layer.out(isignal)
@@ -66,26 +66,44 @@ class NeuralNet(object):
     def predict(self,inputsignals):
         output = np.zeros([len(inputsignals),len(self.layer[-1].post)])
         for i,inputsignal in enumerate(inputsignals):
-            output[i,:] = feedforward(inputsignal)
+            output[i,:] = self.feedforward(inputsignal)
+        return output
             
 
 def run():
+    def class_func(x):
+        return 0.01*(x+1)*(x-8)*(x-7)
+
     def classify(x):
-        return x[1] > 0.01*(x[0]+1)*(x[0]-8)*(x[0]+7)
-    input_signal = np.random.random([2,100])*10 - 5
-    output_signal = classify(input_signal).astype(int)
-    test_signal = np.random.random([2,50]) * 10 -5
+        return x[1] > class_func(x[0])
+
+    input_signal = np.random.random([100,2])*20 - 10
+    output_signal = np.zeros(100)
+    for i in range(100):
+        output_signal[i] = classify(input_signal[i]).astype(int)
+    output_signal = output_signal.reshape([len(output_signal),1]) # need to reshape
+    test_signal = np.random.random([50,2]) * 20 - 10
     NN = NeuralNet(2,3,1)
-    NN.fit(input_signal,output_signal)
+    for i in np.arange(1,10,0.1):
+        NN.fit(input_signal,output_signal,learning_rate=0.01/i)
     predict = NN.predict(test_signal)
-    for t,p in zip(test_signal,predict):
-        plt.plot(*t,colar="r" if predict[0] else "b")
+    plt.figure()
+    for data in zip(test_signal, predict):
+        plt.scatter(*data[0], color="r" if data[1] > 0.5 else "b")
+    if False:
+        plt.figure()
+        for data in zip(input_signal, output_signal):
+            plt.scatter(*data[0], color="y" if data[1] > 0.5 else "g")
+        plt.plot(np.arange(-10, 10, 0.1), class_func(np.arange(-10, 10, 0.1)))
     plt.show()
+
 
 def test():
     L1 = Layer(2,3)
     inputsignal = np.random.random(2)
     print L1.out(inputsignal)
+    NN = NeuralNet(2,3,1)
+    print NN.feedforward(inputsignal)
 
 if __name__=="__main__":
-    test()
+    run()
